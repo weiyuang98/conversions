9a0c2a7c639094632ad721ace5f838d416062f8e
Weiyuangfadb <yuang.wei@postgrad.manchester.ac.uk>
2020-11-24 12:16:52 +0000

    Add K-C conversion trace to graph

--- temperature_conversions.m
+++ temperature_conversions.m
@@ -11,3 +11,13 @@ fprintf('Absolute zero is 0K, which is %g%sC.\n', kelvin_to_celsius(0), deg)
 fprintf('Absolute zero is %g%sF.\n', celsius_to_fahrenheit(kelvin_to_celsius(0)), deg)
 
 fprintf('33 %sN is %g%sC.\n', deg, newton_to_celsius(33), deg)
+
+% Plot Fahrenheit vs Celsius
+degC = linspace(0,100,101);
+plot(degC, celsius_to_fahrenheit(degC))
+hold on
+degK = linspace(273, 373, 101);
+plot(kelvin_to_celsius(degK), degK)
+xlabel('Celsius')
+ylabel('Conversion')
+legend('Fahrenheit', 'Kelvin', 'location', 'northwest')
